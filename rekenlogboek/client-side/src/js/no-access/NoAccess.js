ec6ea3c3fb095a8d50a3cda13cb8a8ab926c16af
Michael Holstein <holsteinmp@gmail.com>
2020-11-23 13:22:53 +0100

    Added noAcces page

--- rekenlogboek/client-side/src/js/no-access/NoAccess.js
+++ rekenlogboek/client-side/src/js/no-access/NoAccess.js
@@ -1,5 +1,15 @@
 import React from 'react'
-
+import '../../scss/no-access/NoAccess.scss'
 export default function NoAccess() {
-	return null
+	return (
+		<section>
+			<h1>Oeps! Geen toegang...</h1>
+			<p>
+				Je hebt niet de juiste rechten voor deze applicatie. Neem contact op met
+				de beheerder, of niet...
+			</p>
+
+			<div class="illustration"></div>
+		</section>
+	)
 }
