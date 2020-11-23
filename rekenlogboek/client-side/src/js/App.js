2497b4130a24d246edd83977a539ed9b1ec5bc6f
Michael Holstein <holsteinmp@gmail.com>
2020-11-23 12:18:14 +0100

    Created SignIn page

--- rekenlogboek/client-side/src/js/App.js
+++ rekenlogboek/client-side/src/js/App.js
@@ -1,27 +1,13 @@
 import '../scss/App.scss'
 import Header from '../js/common/components/Header'
-import Jumbotron from '../js/common/components/Jumbotron'
-import InfoContainer from '../js/common/components/InfoContainer'
-import ButtonContainer from '../js/common/components/ButtonContainer'
-import Button from '../js/common/components/Button'
-
+import SignIn from '../js/sign-in/SignIn'
 function App() {
 	const buttonHandler = e => alert('clickkkk.....')
 	return (
 		<div className="app">
 			<Header />
 			<main>
-				{/* Router here  */}
-
-				<Jumbotron>
-					<ButtonContainer
-						icon="archive"
-						description="Het overzicht van alle logboeken bekijken."
-					>
-						<Button color="blue" value="archiveer" handler={buttonHandler} />
-					</ButtonContainer>
-					<InfoContainer />
-				</Jumbotron>
+				<SignIn />
 			</main>
 		</div>
 	)
