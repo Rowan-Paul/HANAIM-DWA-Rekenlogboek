942989cb3d488634642519b1c4d2365188eede9c
Michael Holstein <holsteinmp@gmail.com>
2020-11-26 10:38:40 +0100

    Fixed inconsistency with illustrations

--- rekenlogboek/client-side/src/js/logbook-designer/components/Illustration.js
+++ rekenlogboek/client-side/src/js/logbook-designer/components/Illustration.js
@@ -3,8 +3,13 @@ import '../../../scss/logbook-designer/components/Illustration.scss'
 export default function Illustration(props) {
 	return (
 		<div className="Illustration">
-			<h1>{props.title}</h1>
-			<img src={props.image} alt="image" />
+			<div className="title">
+				<h1>{props.title}</h1>
+			</div>
+
+			<div className="image">
+				<img src={props.image} alt="image" />
+			</div>
 		</div>
 	)
 }
