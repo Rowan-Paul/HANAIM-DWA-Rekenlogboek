67451aa7e059f9c4bfa8233d5b6303dee3e59e06
Michael Holstein <holsteinmp@gmail.com>
2020-12-01 12:55:27 +0100

    Created dynamic header

--- rekenlogboek/client-side/src/js/common/Header.js
+++ rekenlogboek/client-side/src/js/common/Header.js
@@ -1,14 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+
 import '../../scss/common/Header.scss'
-export default function Header() {
+
+function Header(props) {
+	const navHandler = () => {
+		if (props.loggedIn) {
+			switch (props.user.jobTitle) {
+				case 'Leraar':
+					return (
+						<Link to="./overview">
+							<i className="fa fa-home"></i>
+							<span>Overzicht</span>
+						</Link>
+					)
+				case 'Leerling':
+				default:
+					return ''
+			}
+		}
+	}
+
 	return (
 		<header>
-			<div className="return">
-				{/* <i className="fa fa-chevron-left"></i> */}
-				{/* <span>Vorige</span> */}
+			<div className="return">{navHandler()}</div>
+			<div className="title">
+				<span>{props.title}</span>
 			</div>
-			<div className="title">{/* <span>Title</span> */}</div>
-			<div className="user">{/* <span>John doe</span> */}</div>
+			<div className="user">
+				<span>{props.loggedIn && props.user.name}</span>
+			</div>
 		</header>
 	)
 }
+
+const mapStateToProps = state => {
+	return {
+		loggedIn: state.main.loggedIn,
+		user: state.main.user
+	}
+}
+
+export default connect(mapStateToProps)(Header)
--- rekenlogboek/client-side/src/redux/main/reducer.js
+++ rekenlogboek/client-side/src/redux/main/reducer.js
@@ -1,13 +1,14 @@
 import { SAVE_USER } from './types'
 
 const initialMainState = {
+	loggedIn: false,
 	user: {}
 }
 
 export default function mainReducer(state = initialMainState, action) {
 	switch (action.type) {
 		case SAVE_USER:
-			return { ...state, user: action.payload }
+			return { ...state, loggedIn: true, user: action.payload }
 
 		default:
 			return state
