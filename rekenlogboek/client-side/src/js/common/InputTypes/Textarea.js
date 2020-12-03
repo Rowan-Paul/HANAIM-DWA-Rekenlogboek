52d032aed87e02cb1add34f6582832f1bd3f2357
Michael Holstein <holsteinmp@gmail.com>
2020-12-03 16:16:33 +0100

    Created base version of textarea

---
+++ rekenlogboek/client-side/src/js/common/InputTypes/Textarea.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { addInputValue } from '../../../redux/visual/actions'
+
+import '../../../scss/logbook-designer/components/InputTypes/Textarea.scss'
+
+function Textarea(props) {
+	return (
+		<div className="Textarea">
+			<textarea
+				placeholder="Omschrijving toevoegen"
+				onChange={e => props.addInputValue(e.target.value)}
+				value={props.value}
+			></textarea>
+		</div>
+	)
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+		addInputValue: payload => dispatch(addInputValue(payload))
+	}
+}
+
+export default connect(null, mapDispatchToProps)(Textarea)
