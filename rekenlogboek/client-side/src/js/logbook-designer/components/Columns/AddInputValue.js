63de0b0d501f183e2eb5f2dfef8ad8660ca65cc1
Michael Holstein <holsteinmp@gmail.com>
2020-12-03 16:19:18 +0100

    Added style for topbar and inputvalue component

---
+++ rekenlogboek/client-side/src/js/logbook-designer/components/Columns/AddInputValue.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { addInputValue } from '../../../../redux/visual/actions'
+function addInputValueFunc(props) {
+	const [value, setValue] = useState('')
+
+	const addInputValue = e => {
+		e.preventDefault()
+		props.addInputValue(value)
+		setValue('')
+	}
+
+	return (
+		<div className="Block">
+			<h4>Waarde toevoegen :</h4>
+
+			<div className="AddContainer">
+				<form onSubmit={e => addInputValue(e)}>
+					<input
+						onChange={e => setValue(e.target.value)}
+						placeholder="Waardes toevoegen"
+						type="text"
+						value={value}
+					/>
+					<button>
+						<i className="fa fa-plus"></i>
+					</button>
+				</form>
+			</div>
+		</div>
+	)
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+		addInputValue: payload => dispatch(addInputValue(payload))
+	}
+}
+
+export default connect(null, mapDispatchToProps)(addInputValueFunc)
