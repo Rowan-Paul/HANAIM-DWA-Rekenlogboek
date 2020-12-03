5bba0a17c0fa59452c914d6ae4d20a7584a0f2b5
Michael Holstein <holsteinmp@gmail.com>
2020-12-03 16:17:34 +0100

    Updated Column page

--- rekenlogboek/client-side/src/js/logbook-designer/containers/Columns.js
+++ rekenlogboek/client-side/src/js/logbook-designer/containers/Columns.js
@@ -1,83 +1,83 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { connect } from 'react-redux'
-
+import { setColumn } from '../../../redux/visual/actions'
 import Jumbotron from '../../common/Jumbotron'
-import InfoContainer from '../../common/InfoContainer'
-import SelectColumnTypes from '../components/SelectColumnTypes'
 import Button from '../../common/Button'
-import Illustration from '../components/Illustration'
-import { addLogbookColumns } from '../../../redux/logbook/actions'
 
-import Image from '../../../img/illustrations/log_select_question_type.svg'
+import TopBar from '../components/Visual/TopBar'
+import Evaluation from '../../common/InputTypes/Evaluation'
+import VisualFrame from '../components/Visual/VisualFrame'
+import Modal from '../components/Modal'
+import AddColumn from '../components/AddColumn'
+import VisualHeader from '../components/Visual/VisualHeader'
+import ColumnPreview from '../components/Columns/ColumnPreview'
+
 import '../../../scss/logbook-designer/containers/NewLogbook.scss'
+function Columns(props) {
+	const history = useHistory()
+	const [columns, setColumns] = useState(props.columns)
+	const [modal, setModal] = useState(props.modalVisible)
 
-function Page2(props) {
-	const [columnTitle1, setColumnTitle1] = useState(props.title1)
-	const [columnType1, setColumnType1] = useState(props.inputType1)
-
-	const [columnTitle2, setColumnTitle2] = useState(props.title2)
-	const [columnType2, setColumnType2] = useState(props.inputType2)
-
-	const changeTitleHandler = (column, value) => {
-		column === 1 ? setColumnTitle1(value) : setColumnTitle2(value)
-	}
-
-	const changeTypeHandler = (column, value) => {
-		column === 1 ? setColumnType1(value) : setColumnType2(value)
-	}
-
-	let history = useHistory()
-	const nextButtonHandler = () => {
-		const payload = {
-			columns: [
-				{ position: 1, title: columnTitle1, inputType: columnType1 },
-				{ position: 2, title: columnTitle2, inputType: columnType2 }
-			]
-		}
-
-		if (columnTitle1.trim().length > 0 && columnTitle2.trim().length > 0) {
-			props.addLogbookColumns(payload)
-			history.push('./goals')
+	// Verify if all collumns set
+	const verifyCollumns = () => {
+		if (!columns.every(c => c.added)) {
+			alert('Vul eerst alle kolommen in')
 		} else {
-			//TODO: replace this by something less evil than a alert
-			alert('Vul a.u.b. alle velden in.')
+			history.push('./goals')
 		}
 	}
+
+	// useEffect for model / overlay
+	useEffect(() => {
+		setModal(props.modalVisible)
+	}, [props.modalVisible])
+
+	// useEffect for verifying collums added
+	useEffect(() => {
+		setColumns(props.columns)
+	}, [props.columns])
 
 	return (
 		<div className="new-logbook">
+			{modal && (
+				<Modal handler={props.setColumn}>
+					<AddColumn />
+				</Modal>
+			)}
 			<Jumbotron>
-				<div className="vertical-center">
-					<SelectColumnTypes
-						changeTypeHandler={changeTypeHandler}
-						changeTitleHandler={changeTitleHandler}
-						columnTitle1={columnTitle1}
-						columnTitle2={columnTitle2}
-						columnType1={columnType1}
-						columnType2={columnType2}
-					/>
-				</div>
-				<InfoContainer>
-					<Illustration
-						title="Kies de 2 kolomtypes van een logboek."
-						image={Image}
-					/>
-				</InfoContainer>
+				<TopBar title={'Kolommen toevoegen'} group={5} period={1} />
+				<VisualFrame>
+					<VisualHeader />
+					<li className="Row Body">
+						<div>
+							<p>
+								Maak op de volgende pagina de doelen aan om ook hiervoor een
+								preview te krijgen.
+							</p>
+						</div>
+
+						<ColumnPreview position={1} />
+						<ColumnPreview position={2} />
+						<div>
+							<Evaluation />
+						</div>
+					</li>
+				</VisualFrame>
 			</Jumbotron>
 
 			<div className="prev button">
 				<Button
 					color="gray"
 					value="Vorige"
-					handler={() => history.push('./general')}
+					handler={() => props.history.push('./general')}
 				/>
 			</div>
 			<div className="next button">
 				<Button
 					color="blue"
 					value="Volgende"
-					handler={() => nextButtonHandler()}
+					handler={() => verifyCollumns()}
 				/>
 			</div>
 		</div>
@@ -86,17 +86,15 @@ function Page2(props) {
 
 const mapStateToProps = state => {
 	return {
-		title1: state.logbook.columns[0].title,
-		inputType1: state.logbook.columns[0].inputType,
-		title2: state.logbook.columns[1].title,
-		inputType2: state.logbook.columns[1].inputType
+		columns: state.visualState.columns,
+		modalVisible: state.visualState.modal.visible
 	}
 }
 
 const mapDispatchToProps = dispatch => {
 	return {
-		addLogbookColumns: payload => dispatch(addLogbookColumns(payload))
+		setColumn: () => dispatch(setColumn())
 	}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Page2)
+export default connect(mapStateToProps, mapDispatchToProps)(Columns)
