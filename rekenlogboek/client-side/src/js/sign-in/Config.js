856648ac0020ccc8118ddfd080bf8cebd6948976
Rowan-Paul <rpflynn80@gmail.com>
2020-12-07 08:58:23 +0100

    Update last hardcoded URLs
    didn't test so who knows

--- rekenlogboek/client-side/src/js/sign-in/Config.js
+++ rekenlogboek/client-side/src/js/sign-in/Config.js
@@ -1,6 +1,6 @@
 //TODO: replace it with env variable`
 export const config = {
 	clientId: 'e1c0b43f-30fc-491a-ada6-d5edb53b56f3',
-	redirectUri: 'https://localhost:3001/auth',
+	redirectUri: process.env.REACT_APP_ADDRESS + '/auth',
 	scopes: ['user.read']
 }
