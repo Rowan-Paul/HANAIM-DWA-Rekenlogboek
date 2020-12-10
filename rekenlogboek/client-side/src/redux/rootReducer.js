699be9e0e7e296a7b5ccd398d2a57edb54beb774
Rowan-Paul <rpflynn80@gmail.com>
2020-12-10 17:05:34 +0100

    Removed ws-demo redux-demo

--- rekenlogboek/client-side/src/redux/rootReducer.js
+++ rekenlogboek/client-side/src/redux/rootReducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
 
-import demoReducer from './demo/reducer'
 import logbookReducer from './logbook/reducer'
 import mainReducer from './main/reducer'
 import logbookoverviewReducer from './logbookoverview/reducer'
 
 const rootReducer = combineReducers({
-	demo: demoReducer,
 	logbook: logbookReducer,
 	main: mainReducer,
 	logbookoverview: logbookoverviewReducer
