c8cf0be5af12744e1544f6ce620029a32c2fb49e
Stef Schöningh <SJAH.Schoningh@student.han.nl>
2020-12-10 11:18:05 +0100

    Added new logbook columns page test

--- rekenlogboek/client-side/e2e-tests/logbook.e2e.js
+++ rekenlogboek/client-side/e2e-tests/logbook.e2e.js
@@ -1,5 +1,4 @@
 const puppeteer = require('puppeteer')
-
 jest.setTimeout(30000)
 
 describe(`Rekenlogboek`, () => {
@@ -13,9 +12,11 @@ describe(`Rekenlogboek`, () => {
 	beforeAll(async () => {
 		browserA = await puppeteer.launch({
 			headless: false,
-			slowMo: 1000,
+			slowMo: 30,
 			ignoreHTTPSErrors: true,
-			args: [`--window-size=700,800`, `--window-position=0,0`]
+			// args: [`--window-size=800,800`, `--window-position=0,0`]
+			args: ['--start-maximized'],
+			defaultViewport: null
 		})
 		pageA = await browserA.newPage()
 	})
@@ -24,67 +25,39 @@ describe(`Rekenlogboek`, () => {
 		// await browserA.close()
 	})
 
-	xtest(`Go to main page and login with microsoft`, async () => {
-		await pageA.goto(`https://localhost:3001`)
-		const MicrosoftButton = await pageA.$(`.MicrosoftButton`)
-		expect(MicrosoftButton).toBeDefined()
-		await MicrosoftButton.click()
-	})
-
-	xtest(`Login on Microsoft identity`, async () => {
-		const newPagePromise = getNewPageWhenLoaded()
-		await page.click('my-link') //Opens pop-up window
-		const newPage = await newPagePromise
-		newPage = newPage.mainFrame()
-		const element = await newPage.waitForSelector('img')
-		newPage.click(element)
-		function getNewPageWhenLoaded() {
-			return new Promise(x =>
-				browser.once('targetcreated', async target => {
-					const newPage = await target.page()
-					const newPagePromise = new Promise(() =>
-						newPage.once('domcontentloaded', () => x(newPage))
-					)
-					const isPageLoaded = await newPage.evaluate(() => document.readyState)
-					return isPageLoaded.match('complete|interactive')
-						? x(newPage)
-						: newPagePromise
-				})
-			)
-		}
-		// const newPagePromise = new Promise(x =>
-		// 	browser.once('targetcreated', target => x(target.page()))
-		// )
-		// await page.click('#abtn')
-		const popup = await newPagePromise
-		console.log(popup.url())
-		await pageA.waitFor(2000)
-		await pageA.type('input[name=loginfmt]', user.email, {
-			delay: 50
-		})
-		await pageA.waitFor(500)
-		await pageA.click('input[type=submit]')
-		await pageA.waitFor(500)
-		await pageA.click('input[name=passwd]')
-		await pageA.waitFor(500)
-		await pageA.type('input[name=passwd]', user.pass, {
-			delay: 50
-		})
-		await pageA.waitFor(500)
-		await pageA.click('input[type=submit]')
-	})
-
-	test(`Create logbook - Happy path`, async () => {
+	test(`Create logbook: Happy path - general`, async () => {
 		await pageA.goto(`https://localhost:3001/teacher/new-logbook/general`)
 
-		const selectInputs = await pageA.$$eval(`select`, async arr => {
-			arr[0].value = '8'
-			arr[1].value = '3'
-			return arr.map(item => item.value)
+		const selectInputs = await pageA.$$eval(`select`, async selectInputs => {
+			selectInputs[0].value = '8'
+			selectInputs[1].value = '3'
+			return selectInputs.map(item => item.value)
 		})
 		expect(selectInputs).toStrictEqual(['8', '3'])
 
 		await pageA.click(`.next button`)
-		await pageA.waitForTimeout(3000)
+	})
+
+	test(`Create logbook: Happy path - columns (1)`, async () => {
+		const addColumn = async () => {
+			await pageA.$$eval(`.Plus button`, async plusButtons => {
+				plusButtons[0].click()
+			})
+
+			expect(`Modal`).toBeDefined()
+			await pageA.type(`#title`, `Hoe heb je de toets gemaakt?`)
+			// await pageA.select(`#inputType`, `checkboxes`)
+
+			await pageA.type(`#addOption`, `Ik snap het goed`)
+			await pageA.click(`#addBtn`)
+			await pageA.type(`#addOption`, `Ik snap het niet`)
+			await pageA.click(`#addBtn`)
+			await pageA.click(`.bttn.blue`)
+		}
+
+		await addColumn()
+		await addColumn()
+
+		await pageA.click(`.next button`)
 	})
 })
