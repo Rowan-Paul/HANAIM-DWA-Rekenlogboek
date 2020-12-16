16b4e54c90ea908fdc50ec67200185c29477afb4
Michael Holstein <holsteinmp@gmail.com>
2020-12-16 13:30:20 +0100

    Added button to New Logbook saved

--- rekenlogboek/client-side/src/js/logbook-designer/components/LogbookSaved.js
+++ rekenlogboek/client-side/src/js/logbook-designer/components/LogbookSaved.js
@@ -1,22 +1,23 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom'
+import { useHistory, withRouter } from 'react-router-dom'
 
-// import Button from '../../common/Button'
-
+import Button from '../../common/Button'
 import '../../../scss/logbook-designer/components/LogbookSaved.scss'
 
 function LogbookSavedUI() {
+	const history = useHistory()
 	return (
 		<div className="LogbookSaved">
 			<h1>Logboek aangemaakt</h1>
 			<p>
 				Het logboek is aangemaakt {/*en is nu beschikbaar in het overzicht*/}.
 			</p>
-			{/* <Button
+			<Button
 				color="blue"
-				value="Overzicht bekijken"
-				handler={() => props.history.push('../overview')}
-			/> */}
+				value="Terug naar homepage"
+				// value="Overzicht bekijken"
+				handler={() => history.push('../')}
+			/>
 		</div>
 	)
 }
