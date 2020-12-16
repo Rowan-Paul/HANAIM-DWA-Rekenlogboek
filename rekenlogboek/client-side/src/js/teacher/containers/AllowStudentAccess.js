caab99d5c8c63bf2f833161f99bbe72410f27e53
Stef Schöningh <SJAH.Schoningh@student.han.nl>
2020-12-16 15:52:45 +0100

    Added jsx to allowstudentaccess component

--- rekenlogboek/client-side/src/js/teacher/containers/AllowStudentAccess.js
+++ rekenlogboek/client-side/src/js/teacher/containers/AllowStudentAccess.js
@@ -2,8 +2,61 @@ import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 
+import Check from '../../../img/icons/check_green.svg'
+import Lock from '../../../img/icons/lock_blue.svg'
+
+import Jumbotron from '../../common/Jumbotron'
+import ButtonContainer from '../../common/ButtonContainer'
+import Button from '../../common/Button'
+
 function AllowStudentAccess(props) {
-	return <div></div>
+	return (
+		<div className="allow-student-access">
+			<div className="period-filter">
+				<div>Blok:</div>
+				<input type="number" value="1" min="1" max="100" />
+				<Button color="blue" handler={() => {}}>
+					Kies blok
+				</Button>
+			</div>
+			<Jumbotron>
+				<div className="content-container">
+					<div className="explanation-container">
+						<h1>Bepaal wat uw leerlingen zien.</h1>
+						<p>
+							Kies welk deel van het logboek u wilt ontgrendelen. Er kan maar
+							een gedeelte tegelijkertijd open staan. Bij het kiezen van de
+							evaluaties pagina kunt u zelf bepalen welke leerdoelen geëvalueerd
+							mogen worden.
+						</p>
+					</div>
+					<ButtonContainer
+						icon={Check}
+						color="green"
+						description="Open na pre-toets pagina."
+						value="Ontgrendel"
+						// handler={() => history.push('../teacher/logbooks')}
+					/>
+
+					<ButtonContainer
+						icon={Lock}
+						color="blue"
+						description="Sluit instructie pagina."
+						value="Vergrendel"
+						// handler={() => history.push('../teacher/logbooks')}
+					/>
+
+					<ButtonContainer
+						icon={Check}
+						color="green"
+						description="Open na pre-toets pagina."
+						value="Ontgrendel"
+						// handler={() => history.push('../teacher/logbooks')}
+					/>
+				</div>
+			</Jumbotron>
+		</div>
+	)
 }
 
 const mapStateToProps = state => {
