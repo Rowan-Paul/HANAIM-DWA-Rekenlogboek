e42c522147a770d59ddd6b860d9bb6199c7b0450
Sjaak <sjaakrest@gmail.com>
2020-12-17 11:08:42 +0100

    remove import

--- rekenlogboek/client-side/src/js/student/containers/Default.js
+++ rekenlogboek/client-side/src/js/student/containers/Default.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import '../../../scss/student/containers/Default.scss'
