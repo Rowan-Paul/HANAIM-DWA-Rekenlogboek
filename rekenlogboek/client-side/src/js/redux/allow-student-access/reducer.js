032873d766f70b06259cb1c550ddd3a514269ae0
Stef Schöningh <SJAH.Schoningh@student.han.nl>
2020-12-17 14:39:57 +0100

    Added allow-student-access reducer

--- rekenlogboek/client-side/src/js/redux/allow-student-access/reducer.js
+++ rekenlogboek/client-side/src/js/redux/allow-student-access/reducer.js
@@ -2,5 +2,32 @@ import * as types from './types'
 
 const INITIAL_STATE = {}
 
-const reducer = (state = INITIAL_STATE, action) => {}
+const reducer = (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+		case types.GET_ACTIVE_LOGBOOK:
+			return {
+				...state
+			}
+		case types.GET_ACTIVE_LOGBOOK_SUCCESS:
+			return {
+				...state
+			}
+		case types.GET_YEARS:
+			return {
+				...state
+			}
+		case types.GET_YEARS_SUCCESS:
+			return {
+				...state
+			}
+		case types.UPDATE_LOGBOOK:
+			return {
+				...state
+			}
+		case types.UPDATE_LOGBOOK_SUCCESS:
+			return {
+				...state
+			}
+	}
+}
 export default reducer
--- rekenlogboek/client-side/src/js/redux/rootReducer.js
+++ rekenlogboek/client-side/src/js/redux/rootReducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux'
 import logbookReducer from './logbook/reducer'
 import mainReducer from './main/reducer'
 import logbookoverviewReducer from './logbookoverview/reducer'
+import allowStudentAccessReducer from './allow-student-access/reducer'
 
 const rootReducer = combineReducers({
 	logbook: logbookReducer,
 	main: mainReducer,
-	logbookoverview: logbookoverviewReducer
+	logbookoverview: logbookoverviewReducer,
+	allowStudentAccess: allowStudentAccessReducer
 })
 
 export default rootReducer
