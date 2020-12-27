93d5a14c8863526fd4c54e14e998d9e4475ce9ea
Michael Holstein <holsteinmp@gmail.com>
2020-12-27 13:59:05 +0100

    Created main frame for logbook overview

--- rekenlogboek/client-side/src/js/teacher/containers/group-overview/Index.js
+++ rekenlogboek/client-side/src/js/teacher/containers/group-overview/Index.js
@@ -1,13 +1,51 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 
+import Jumbotron from '../../../common/Jumbotron'
+import TopBar from '../../../common/logbook/TopBar'
+import LogbookFrame from '../../../common/logbook/LogbookFrame'
+import LogbookRows from '../../../common/logbook/LogbookRows'
+
+import { getLogbook } from '../../../redux/group-overview/actions'
 import '../../../../scss/teacher/containers/group-overview/Index.scss'
-export const Index = () => {
-	return <div className="GroupOverview"></div>
+export const Index = props => {
+	const [logbook, setLogbook] = useState(props.logbook)
+
+	const logbookHandler = () => {
+		if (!logbook) {
+			props.getLogbook()
+			return ''
+		} else {
+			return (
+				<LogbookRows
+					type={props.logbookTypes.groupOverview}
+					goals={logbook.goals}
+				/>
+			)
+		}
+	}
+
+	useEffect(() => {
+		setLogbook(props.logbook)
+	}, [props.logbook])
+
+	return (
+		<div className="GroupOverview">
+			<Jumbotron>
+				<TopBar title="Groepsoverzicht logboek" noBreadcrumbs />
+				<LogbookFrame>{logbookHandler()}</LogbookFrame>
+			</Jumbotron>
+		</div>
+	)
 }
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({
+	logbookTypes: state.main.logbookTypes,
+	logbook: state.groupOverview.logbook
+})
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = dispatch => ({
+	getLogbook: () => dispatch(getLogbook())
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(Index)
