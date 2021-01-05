11e8c30596fa40af8d9ce0186c97d5f99d2315ff
Sjaak <sjaakrest@gmail.com>
2021-01-05 13:05:09 +0100

    add instructions end page

---
+++ rekenlogboek/client-side/src/js/student/containers/InstructionsEnd2.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+
+import '../../../scss/student/Student.scss'
+
+import ProgressBar from '../components/ProgressBar'
+import ResultText from '../components/ResultText'
+import ResultTable from '../components/ResultTable'
+
+import Jumbotron from '../../common/Jumbotron'
+import Button from '../../common/Button'
+
+function InstructionsEndUI(props) {
+	const previousPage = () => {
+		props.history.push('/student/instruction')
+	}
+
+	return (
+		<div className="end-screen student-container">
+			{/* <ProgressBar itemCount={props.goalAmount} done={[0, 1, 2, 3, 4]} /> */}
+			<Jumbotron columns={1}>
+				<div className="learn-goal-container">
+					<div className="left-side">
+						<ResultText
+							title="Je bent klaar!"
+							description="De door jou ingevoerde antwoorden zijn naar jouw leraar verstuurd."
+							image=""
+						/>
+					</div>
+					<div className="right-side">
+						<ResultTable
+							results={props.allGoals}
+							answers={props.answers}
+							columnPosition={2}
+							description="Dit heb je bij je leerdoelen beantwoord:"
+						/>
+					</div>
+				</div>
+			</Jumbotron>
+			<div className="prev button">
+				<Button color="gray" value="Vorige" handler={() => previousPage()} />
+			</div>
+		</div>
+	)
+}
+
+function mapStateToProps(state) {
+	return {
+		answers: state.studentLogbook.studentlogbook.answers,
+		allGoals: state.studentLogbook.logbook.goals
+	}
+}
+
+export const InstructionsEnd = connect(
+	mapStateToProps,
+	null
+)(withRouter(InstructionsEndUI))
