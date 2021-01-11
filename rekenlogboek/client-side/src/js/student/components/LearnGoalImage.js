0561c596d08de97ddcdf25043381891337844e04
Sjaak <sjaakrest@gmail.com>
2021-01-11 11:46:25 +0100

    only add learngoal image when image link exists

--- rekenlogboek/client-side/src/js/student/components/LearnGoalImage.js
+++ rekenlogboek/client-side/src/js/student/components/LearnGoalImage.js
@@ -1,18 +1,23 @@
 import React from 'react'
 
-// import a from '../../../img/temp/LearnGoalThumb.png'
-
 export default function LearnGoalImage(props) {
+	const loadImage = () => {
+		if (props.src) {
+			let image = new Image()
+			let imageURL =
+				process.env.REACT_APP_SERVER_ADDRESS + '/uploads/goals/' + props.src
+			image.src = imageURL
+			if (image.width !== 0) {
+				return <img src={imageURL} alt="leerdoel afbeelding" />
+			}
+		}
+	}
+
 	return (
 		<div className="learn-goal-image">
 			<h2>{props?.title}</h2>
 			<p>{props?.description}</p>
-			<img
-				src={
-					process.env.REACT_APP_SERVER_ADDRESS + '/uploads/goals/' + props.src
-				}
-				alt="leerdoel afbeelding"
-			/>
+			{loadImage()}
 		</div>
 	)
 }
